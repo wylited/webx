c084338cd66a3766ec47a37ee59bcccc9bfbf686
wylited <44154103+wylited@users.noreply.github.com>
2024-07-24 17:58:14 +0530

    initial homepage ui

--- src/models.rs
+++ src/models.rs
@@ -1,24 +1,8 @@
-use serde::{Deserialize, Serialize};
+use serde::{Serialize};
 
 #[derive(Clone, Serialize, Debug)]
 pub enum MutationKind {
     Create,
     Delete,
-}
-
-#[derive(Clone, Serialize, Debug)]
-pub struct TodoUpdate {
-    pub mutation_kind: MutationKind,
-    pub id: i32,
-}
-
-#[derive(sqlx::FromRow, Serialize, Deserialize)]
-pub struct Todo {
-    pub id: i32,
-    pub description: String,
-}
-
-#[derive(sqlx::FromRow, Serialize, Deserialize)]
-pub struct TodoNew {
-    pub description: String,
+    Update,
 }
--- src/router.rs
+++ src/router.rs
@@ -1,13 +1,9 @@
-use crate::models::TodoUpdate;
 use axum::{
-    routing::{delete, get},
-    Extension, Router,
+    routing::{get}, Router,
 };
 use sqlx::PgPool;
 
 use crate::routes;
-use tokio::sync::broadcast::{channel, Sender};
-pub type TodosStream = Sender<TodoUpdate>;
 
 #[derive(Clone)]
 pub struct AppState {
@@ -15,16 +11,10 @@ pub struct AppState {
 }
 
 pub fn init_router(db: PgPool) -> Router {
-    let (tx, _rx) = channel::<TodoUpdate>(10);
     let state = AppState { db };
 
     Router::new()
         .route("/", get(routes::home))
-        .route("/stream", get(routes::stream))
-        .route("/styles.css", get(routes::styles))
-        .route("/todos", get(routes::fetch_todos).post(routes::create_todo))
-        .route("/todos/:id", delete(routes::delete_todo))
-        .route("/todos/stream", get(routes::handle_stream))
+        .route("/output.css", get(routes::styles))
         .with_state(state)
-        .layer(Extension(tx))
 }
